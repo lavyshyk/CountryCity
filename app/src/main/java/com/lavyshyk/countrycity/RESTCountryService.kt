ef041352b7a0226eaa98206a8bfeb3e4278f9f50
Pavel Lavyshyk <lavyshyk@gmail.com>
2021-07-03 22:07:59 +0300

    fix - add LoggingInterceptor

--- app/src/main/java/com/lavyshyk/countrycity/RESTCountryService.kt
+++ app/src/main/java/com/lavyshyk/countrycity/RESTCountryService.kt
@@ -1,10 +1,13 @@
 package com.lavyshyk.countrycity
 
 import com.lavyshyk.countrycity.data.CountryData
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
 
 interface RESTCountryService {
 
@@ -14,9 +17,14 @@ interface RESTCountryService {
 }
 const val BASE_URL = "https://restcountries.eu/rest/v2/"
 
+var logging:HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+var httpClient: OkHttpClient.Builder =  OkHttpClient().newBuilder().addInterceptor(logging)
+
 val retrofit: Retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addConverterFactory(GsonConverterFactory.create())
+    .client(httpClient.build())
     .build()
 
 object CountryApi {
